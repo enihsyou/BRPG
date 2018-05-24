7b66197b6d4fcd70fa1d0cbf8814ac17f893b615
ReggieTAT <949205114@qq.com>
2018-05-24 08:03:09 +0800

    添加javax编译时依赖，实现servlet基类

--- src/main/java/servlet/BaseServlet.java
+++ src/main/java/servlet/BaseServlet.java
@@ -1,4 +1,24 @@
 package servlet;
 
-public class BaseServlet {
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class BaseServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        try {
+            Class clazz=this.getClass();
+            String methodName=req.getParameter("method");
+            Method method=clazz.getMethod(methodName,HttpServletRequest.class,HttpServletResponse.class);
+            method.invoke(this,req,res);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
