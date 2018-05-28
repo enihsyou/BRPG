e526cf29be48b681e5863c0071da5179d6abb684
ReggieTAT <949205114@qq.com>
2018-05-28 21:07:56 +0800

    补了dao层和javabean

--- src/main/java/javabean/Game.java
+++ src/main/java/javabean/Game.java
@@ -6,5 +6,53 @@ public class Game {
     private String synopsis;
     private String image;
     private float averageScore;
+    private GameCommentList gameCommentList;
 
+    public String getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(String gameID) {
+        this.gameID = gameID;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public float getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(float averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    public GameCommentList getGameCommentList() {
+        return gameCommentList;
+    }
+
+    public void setGameCommentList(GameCommentList gameCommentList) {
+        this.gameCommentList = gameCommentList;
+    }
 }
