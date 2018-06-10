bd4eae7fc5b6cef64862978912065496dfcdbf3b
ReggieTAT <949205114@qq.com>
2018-06-10 10:42:35 +0800

    游戏详情页面测试数据

--- src/main/java/servlet/StupidServlet.java
+++ src/main/java/servlet/StupidServlet.java
@@ -2,6 +2,9 @@ package servlet;
 
 import com.google.gson.Gson;
 import javabean.Game;
+import javabean.GameComment;
+import javabean.GameCommentList;
+import javabean.User;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -41,6 +44,37 @@ public class StupidServlet extends BaseServlet {
         }
         Gson gson=new Gson();
         String json=gson.toJson(gameList);
+        res.getWriter().write(json);
+    }
+
+    /**
+     * 读取游戏详情
+     * @param req
+     * @param res 返回数据为Game对象的Json串
+     * @throws Exception
+     */
+    public void getGameInfo(HttpServletRequest req,HttpServletResponse res)throws Exception{
+        Game game=new Game();
+        game.setGameID("10001");
+        game.setGameName("嘻嘻哈哈");
+        game.setSynopsis("我是一个小朋友小呀小朋友，今年二十又七八又呀又七八");
+        game.setImage("https://cdn.steamstatic.com.8686c.com/steam/apps/846470/capsule_184x69.jpg?t=1528390713");
+        game.setAverageScore(Float.parseFloat("4.5"));
+        GameCommentList gameCommentList=new GameCommentList();
+        List<GameComment> gameComments=new ArrayList<>();
+        for (int i = 0; i <10 ; i++) {
+            GameComment gameComment=new GameComment();
+            gameComment.setContent("今天天气好");
+            gameComment.setTime("2018-08-29 15:28");
+            User user=new User();
+            user.setUserName("NameBob"+i);
+            gameComment.setUser(user);
+            gameComments.add(gameComment);
+        }
+        gameCommentList.setGameCommentList(gameComments);
+        game.setGameCommentList(gameCommentList);
+        Gson gson=new Gson();
+        String json=gson.toJson(game);
         res.getWriter().write(json);
     }
 }
